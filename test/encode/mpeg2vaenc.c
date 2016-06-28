c1f2944707a583ff304eeb43726b25f32a3313ce
Lim Siew Hoon <siew.hoon.lim@intel.com>
2016-06-28 10:29:43 +0800

    add assert check for NULL if bs->buffer fail to get memory allocation
    Signed-off-by: Lim Siew Hoon <siew.hoon.lim@intel.com>

--- test/encode/mpeg2vaenc.c
+++ test/encode/mpeg2vaenc.c
@@ -216,6 +216,7 @@ bitstream_put_ui(bitstream *bs, unsigned int val, int size_in_bits)
         if (pos + 1 == bs->max_size_in_dword) {
             bs->max_size_in_dword += BITSTREAM_ALLOCATE_STEPPING;
             bs->buffer = realloc(bs->buffer, bs->max_size_in_dword * sizeof(unsigned int));
+            assert(bs->buffer);
         }
 
         bs->buffer[pos + 1] = val;
